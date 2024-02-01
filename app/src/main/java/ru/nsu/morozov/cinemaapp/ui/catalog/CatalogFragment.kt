802b0f62d26d14dbca11886e97eedeeacb1b7c7a
Ruslan Morozov <r.morozov@g.nsu.ru>
2024-02-02 00:34:41 +0700

    add styles and paddings

--- app/src/main/java/ru/nsu/morozov/cinemaapp/ui/catalog/CatalogFragment.kt
+++ app/src/main/java/ru/nsu/morozov/cinemaapp/ui/catalog/CatalogFragment.kt
@@ -1,11 +1,10 @@
 package ru.nsu.morozov.cinemaapp.ui.catalog
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -59,20 +58,32 @@ class CatalogFragment : Fragment() {
 
     private fun showProgress() {
         with(binding) {
-            //Toast.makeText(context, "Loading today films", Toast.LENGTH_SHORT).show()
+            errorContent.isVisible = false
+            recyclerView.isVisible = false
+            progressBar.isVisible = true
         }
     }
 
     private fun showContent(films: List<Film>) {
         with(binding) {
+            progressBar.isVisible = false
+            errorContent.isVisible = false
+            recyclerView.isVisible = true
+
             adapter.submitList(films)
         }
     }
 
     private fun showError(message: String) {
         with(binding) {
-            Log.d("JSONERROR", message)
-            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+            progressBar.isVisible = false
+            recyclerView.isVisible = false
+            errorContent.isVisible = true
+
+            errorText.text = message
+            errorButton.setOnClickListener {
+                viewModel.loadData()
+            }
         }
     }
 
